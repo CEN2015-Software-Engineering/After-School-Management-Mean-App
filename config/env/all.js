5107149403c7be553681ae0e17b1cded793d1048
josh1289op <josh1289op@gmail.com>
2015-02-06 13:29:13 -0500

    fixing calendar text

--- config/env/all.js
+++ config/env/all.js
@@ -25,7 +25,8 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/moment/moment.js'
 			]
 		},
 		css: [
