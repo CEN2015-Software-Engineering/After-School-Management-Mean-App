422514b3864baa5b40fd2732605d08c0003b0e6a
Josh1289op <Joshuakegley@gmail.com>
2015-02-07 16:14:24 -0500

    Added modal for editing guardians, cleaned up creating a child page, adding support for create guardian model, added delete confirmation, added delete and edit buttons to list view

--- public/modules/guardians/controllers/guardians.client.controller.js
+++ public/modules/guardians/controllers/guardians.client.controller.js
@@ -1,9 +1,87 @@
 'use strict';
 
 // Guardians controller
-angular.module('guardians').controller('GuardiansController', ['$scope', '$stateParams', '$location', 'Guardians','$modal', '$log',
-	function($scope, $stateParams, $location, Guardians, $modal, $log) {
+angular.module('guardians').controller('GuardiansController', ['$scope', '$window', '$stateParams', '$location', 'Guardians','$modal', '$log',
+	function($scope, $window, $stateParams, $location, Guardians, $modal, $log) {
 
+
+		// Remove existing Guardian
+		this.remove = function(guardian) {
+			if($window.confirm('Are you sure you want to delete ' + guardian.gName + ' ?')) {
+				if ( guardian ) {
+					guardian.$remove();
+
+					for (var i in $scope.guardians) {
+						if ($scope.guardians [i] === guardian) {
+							$scope.guardians.splice(i, 1);
+						}
+					}
+				} else {
+					$scope.guardian.$remove(function() {
+						$location.path('guardians');
+					});
+				}
+			}
+		};
+
+		// Find a list of Guardians
+		$scope.find = function() {
+			$scope.guardians = Guardians.query();
+		};
+
+		// Find existing Guardian
+		$scope.findOne = function() {
+			$scope.guardian = Guardians.get({
+				guardianId: $stateParams.guardianId
+			});
+		};
+
+		//find Guardians by ChildID
+		$scope.findByChildID = function(id) {
+			console.log($scope);
+			$scope.guardians = Guardians.query();
+			console.log(id);
+		};
+
+
+
+
+		//Open Modal Window to Add Guardian
+		this.modalUpdate = function (size, selectedGuardian) {
+
+			var modalInstance = $modal.open({
+				templateUrl: '/modules/guardians/views/edit-guardian.client.view.html',
+				controller: function ($scope, $modalInstance, $stateParams, guardian) {
+					$scope.guardian = guardian;
+
+
+					$scope.ok = function () {
+						$modalInstance.close($scope.guardian);
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
+				},
+				size: size,
+				resolve: {
+					guardian: function() {
+						return selectedGuardian;
+					}
+				}
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+				$scope.selected = selectedItem;
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
+	}
+]);
+
+angular.module('guardians').controller('GuardiansCreateController', ['$scope', '$stateParams', '$location', 'Guardians',
+	function($scope, $stateParams, $location, Guardians) {
 		// Create new Guardian
 		$scope.create = function() {
 			// Create new Guardian object
@@ -23,96 +101,28 @@ angular.module('guardians').controller('GuardiansController', ['$scope', '$state
 			});
 		};
 
+		$scope.relationships = ['Mom', 'Dad', 'Sister', 'Brother', 'Aunt', 'Uncle', 'Grandma', 'Grandpa', 'Other'];
 
+	}
+]);
 
-		// Remove existing Guardian
-		$scope.remove = function(guardian) {
-			if ( guardian ) { 
-				guardian.$remove();
-
-				for (var i in $scope.guardians) {
-					if ($scope.guardians [i] === guardian) {
-						$scope.guardians.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.guardian.$remove(function() {
-					$location.path('guardians');
-				});
-			}
-		};
-
+angular.module('guardians').controller('GuardiansUpdateController', ['$scope', '$stateParams', '$location', 'Guardians',
+	function($scope, $stateParams, $location, Guardians) {
 		// Update existing Guardian
-		$scope.update = function() {
-			var guardian = $scope.guardian;
+		this.update = function(updatedGuardian) {
+			var guardian = updatedGuardian;
 
 			guardian.$update(function() {
-				$location.path('guardians/' + guardian._id);
+				//this updates the guardian
+
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
 		};
 
-		// Find a list of Guardians
-		$scope.find = function() {
-			$scope.guardians = Guardians.query();
-		};
+		$scope.relationships = ['Mom', 'Dad', 'Sister', 'Brother', 'Aunt', 'Uncle', 'Grandma', 'Grandpa', 'Other'];
 
-		// Find existing Guardian
-		$scope.findOne = function() {
-			$scope.guardian = Guardians.get({ 
-				guardianId: $stateParams.guardianId
-			});
-		};
-
-		//find Guardians by ChildID
-		$scope.findByChildID = function(id) {
-			console.log($scope);
-			$scope.guardians = Guardians.query();
-			console.log(id);
-		};
-		//Open Modal Window to Add Guardian
-		$scope.modalCreate = function (size) {
-
-			var modalInstance = $modal.open({
-				templateUrl: '/modules/guardians/views/create-guardian-child-profile.client.view.html',
-				controller: function ($scope, $modalInstance, $stateParams) {
-					// Create new Guardian
-					$scope.createByChildId = function() {
-						// Create new Guardian object
-						var guardian = new Guardians ({
-							gName: this.gName,
-							childID: $stateParams.childId
-						});
-
-						// Redirect after save
-						guardian.$save(function(response) {
-
-							$modalInstance.close();
-
-						}, function(errorResponse) {
-							$scope.error = errorResponse.data.message;
-						});
-					};
-
-					$scope.submit = function() {
-						$modalInstance.close();
-						$scope.find();
-					};
-
-					$scope.cancel = function() {
-						$modalInstance.dismiss('cancel');
-					};
-
-				},
-				size: size
-			});
-
-			modalInstance.result.then(function (selectedItem) {
-				$scope.selected = selectedItem;
-			}, function () {
-				$log.info('Modal dismissed at: ' + new Date());
-			});
-		};
+		$scope.guardians = Guardians.query();
 	}
 ]);
+
--- public/modules/guardians/services/guardians.client.service.js
+++ public/modules/guardians/services/guardians.client.service.js
@@ -1,13 +1,33 @@
 'use strict';
 
 //Guardians service used to communicate Guardians REST endpoints
-angular.module('guardians').factory('Guardians', ['$resource',
-	function($resource) {
-		return $resource('guardians/:guardianId', { guardianId: '@_id'
-		}, {
-			update: {
-				method: 'PUT'
-			}
-		});
-	}
-]);
+angular.module('guardians')
+	.factory('Guardians', ['$resource', function($resource) {
+			return $resource('guardians/:guardianId', { guardianId: '@_id'
+			}, {
+				update: {
+					method: 'PUT'
+				}
+			});
+		}
+	])
+	.factory('Notify', ['$rootScope', function($rootScope) {
+			//When we save a new customer from the model, it will send information to the main view to update the scope
+			var notify = {};
+
+			notify.sendMsg = function(msg, data) {
+				data = data || {};
+				$rootScope.$emit(msg, data);
+				console.log('message sent');
+			};
+
+			notify.getMsg = function(msg, func, scope){
+				var unbind = $rootScope.$on(msg, func);
+
+				if( scope ) {
+					scope.$on('destory', unbind);
+				}
+			};
+		}
+
+	]);
