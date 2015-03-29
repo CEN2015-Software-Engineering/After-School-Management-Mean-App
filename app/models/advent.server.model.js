e67223b64fd75553d38c03f8ffdb7a4eafc939d8
unknown <alewitt2@gmail.com>
2015-03-29 12:53:30 -0400

    require advent creation stuff

--- app/models/advent.server.model.js
+++ app/models/advent.server.model.js
@@ -22,23 +22,29 @@ var AdventSchema = new Schema({
 	},
 	date: {
 		day: {
+			required: 'Please add day',
 			type: Number
 		},
 		month: {
+			required: 'Please add month',
 			type: Number
 		},
 		year: {
+			required: 'Please add year',
 			type: Number
 		}
 	},
     time:{
         hr: {
+        	required: 'Please add hour',
             type: Number
         },
         min: {
+        	required: 'Please add minute',
             type: Number
         },
         timeSuffix: {
+        	required: 'Please add AM/PM',
             type: String,
             default: 'PM'
         }
